59c9bf1456de2c976606f3f276b933c30d3f3c9f
Raúl Carboneras <racarb@microsoft.com>
2026-01-22 14:55:07 +0100

    Enhance output formatting and logging in license script
    Updated the script to format output tables and enhance logging for license changes.

--- ArcSQLSetLicenses/SetLicensestoExistingServersReclass.ps1
+++ ArcSQLSetLicenses/SetLicensestoExistingServersReclass.ps1
@@ -1,6 +1,5 @@
 #This script will set the license type to the specified one, for all SQL servers Enterprise and Standard Edition
 # that have the SQL extension installed
-
 #Updated settings object
 $LicenseType = "Paid" # Paid,PAYG
 $Settings = @{ SqlManagement = @{ IsEnabled = $true }; LicenseType = $LicenseType }
@@ -42,23 +41,60 @@ $sqlextensionsconf = $sqlextensions | ForEach-Object {
 }
 
 # Show results
-Write-Host "Servers with SQL extension installed and with license type $($LicenseType):"
-$sqlextensionsconf | Where-Object { ($_.LicenseType -like $LicenseType) -and ($_.edition -in @("Standard", "Enterprise")) } | Sort-Object -Property Edition
-Write-Host "Servers with SQL extension installed and with license type not set to $($LicenseType):"
-$sqlextensionsconf | Where-Object { ($_.LicenseType -notlike $LicenseType) -and ($_.edition -in @("Standard", "Enterprise")) } | Sort-Object -Property Edition
+Write-Host "Servers with SQL extension installed and with license type set to $($LicenseType):" -ForegroundColor Green
+$sqlextensionsconf | Where-Object { ($_.LicenseType -like $LicenseType) -and ($_.edition -in @("Standard", "Enterprise")) } | Sort-Object -Property Edition | Format-Table
+Write-Host "Servers with SQL extension installed and with license type NOT set to $($LicenseType):" -ForegroundColor Yellow
+$sqlextensionsconf | Where-Object { ($_.LicenseType -notlike $LicenseType) -and ($_.edition -in @("Standard", "Enterprise")) }  | Sort-Object -Property Edition | Format-Table
 
 # Set extensions that don't have license type set to paid
-$extensionstoset = $sqlextensions | Where-Object { $_.Setting["LicenseType"] -ne $LicenseType}
+$extensionstoset = $sqlextensions | Where-Object { 
+    $id = ($_.id -replace "/extensions/WindowsAgent.SqlServer").tolower()
+    $edition = $SQLInstanceshash["$id"].edition
+    ($_.Setting.AdditionalProperties.LicenseType -ne $LicenseType) -and ($edition -in @("Standard", "Enterprise"))
+}
 
-break # Remove this line to run the script to make changes
+# Check if there are any extensions to update
+if ($extensionstoset.Count -eq 0) {
+    Write-Host "All extensions are already set to `"$($LicenseType)`"" -ForegroundColor Green
+    exit
+}
+
+# Confirmation prompt
+Write-Warning "Are you sure you want to make changes to the above extensions and set their license to $($LicenseType)?"
+$confirmation = Read-Host "Press ENTER to continue or any other key or CTRL+C to cancel"
+if ($confirmation -ne "") {
+    Write-Host "Operation cancelled" -ForegroundColor Red
+    exit
+}
+
+# Create log file with timestamp
+$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+$logFile = "LicenseChanges_$timestamp.log"
+"License Type Changes Log - $(Get-Date)" | Out-File -FilePath $logFile
+"=" * 80 | Out-File -FilePath $logFile -Append
+"`n" | Out-File -FilePath $logFile -Append
 
 foreach ($item in $extensionstoset) {
     $ResourceGroup = ($item.id -split "/")[-7]
     $Machinename = ($item.id -split "/")[-3]
     $location = $item.Location
+    $id = ($item.id -replace "/extensions/WindowsAgent.SqlServer").tolower()
+    $edition = $SQLInstanceshash["$id"].edition
+    $oldLicenseType = $item.Setting.AdditionalProperties.LicenseType
+    
+    # Log the change
+    "Server: $Machinename" | Out-File -FilePath $logFile -Append
+    "Resource Group: $ResourceGroup" | Out-File -FilePath $logFile -Append
+    "Location: $location" | Out-File -FilePath $logFile -Append
+    "Edition: $edition" | Out-File -FilePath $logFile -Append
+    "License Type BEFORE: $oldLicenseType" | Out-File -FilePath $logFile -Append
+    "License Type AFTER: $LicenseType" | Out-File -FilePath $logFile -Append
+    "Extension ID: $($item.id)" | Out-File -FilePath $logFile -Append
+    "Timestamp: $(Get-Date)" | Out-File -FilePath $logFile -Append
+    "-" * 80 | Out-File -FilePath $logFile -Append
+    
     Set-AzConnectedMachineExtension -Name "WindowsAgent.SqlServer" -ResourceGroupName $ResourceGroup -MachineName $Machinename `
         -Location $location -Publisher "Microsoft.AzureData" -Setting $Settings -ExtensionType "WindowsAgent.SqlServer" -NoWait
 }
 
-
-
+Write-Host "`nChanges logged to: $logFile" -ForegroundColor Cyan
